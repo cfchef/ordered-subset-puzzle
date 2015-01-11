33c33e0f8cc1b225302c5ebfa25c6f5a1942a684
Tony Junkes <tony@coldfusionchef.com>
2015-01-11 18:25:39 -0500

    Added a example in Groovy

---
+++ groovy/orderedSubset.groovy
@@ -0,0 +1,39 @@
+def set = [
+    one : [
+        alphabet : "a",
+        maori : "tahi",
+        roman : "i",
+        ordinal : "first"
+    ],
+    two : [
+        alphabet : "b",
+        maori : "rua",
+        roman : "ii",
+        ordinal : "second"
+    ],
+    three : [
+        alphabet : "c",
+        maori : "toru",
+        roman : "iii",
+        ordinal : "third"
+    ],
+    four : [
+        alphabet : "d",
+        maori : "wha",
+        roman : "iv",
+        ordinal : "fourth"
+    ]
+]
+
+def subset = ["four", "two"]
+
+def List orderedSubset(set, subset) {
+    List list = []
+    subset.each { e ->
+        set.find { set.containsKey(e) }.with { set[e].label = e; list << set[e] }
+    }
+    
+    return list
+}
+
+println(orderedSubset(set, subset))
