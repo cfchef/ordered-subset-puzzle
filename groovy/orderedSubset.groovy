214e45a274727342c4207d22d17b119eecf2000d
Tony Junkes <tony@coldfusionchef.com>
2015-01-11 22:46:58 -0500

    update

--- groovy/orderedSubset.groovy
+++ groovy/orderedSubset.groovy
@@ -29,7 +29,7 @@ def subset = ["four", "two"]
 
 List orderedSubset(Map set, List subset) {
     List list = []
-    subset.findAll { set.containsKey(it) }.each { e -> set[e]?.label = e; list << set[e] }
+    subset.findAll { set.containsKey(it) }.each { set[it]?.label = it; list << set[it] }
     
     return list
 }
